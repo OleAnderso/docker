0dd19948d757c8dd46a72cb72d674f6123591671
OleAnderso <101440312+OleAnderso@users.noreply.github.com>
2022-03-12 22:59:14 +0100

    Update script_docker_pull.sh

--- script_docker_pull.sh
+++ script_docker_pull.sh
@@ -5,15 +5,15 @@ echo "RunCount: $end"
 count=0
 while [ $SECONDS -lt $end ]; do
     echo "Count: $count"
-    docker pull --quiet michaelknightdriver/docker-nginx-brotli 
-    docker image rm michaelknightdriver/docker-nginx-brotli > /dev/null
+    docker pull --quiet "$1"
+    docker image rm "$1" > /dev/null
     ((count++))
     #if (( count > 49 )); then
-    #    curl  -X POST -H "Content-type: application/json" -d "{ \"series\" : [{\"metric\":\"docker.pull.dockerhub\", \"points\":[[$currenttime, $count]], \"type\":\"count\", \"host\":\"test.example.com\", \"tags\":[\"environment:test\"]} ]  }" 'https://app.datadoghq.com/api/v1/series?api_key=45814898a6681cc7e7e10086d3cae350'
+    #    curl  -X POST -H "Content-type: application/json" -d "{ \"series\" : [{\"metric\":\"docker.pull.dockerhub\", \"points\":[[$currenttime, $count]], \"type\":\"count\", \"host\":\"test.example.com\", \"tags\":[\"environment:test\"]} ]  }" "https://app.datadoghq.com/api/v1/series?api_key=$2"
     #    count=$((count-50))
     # fi
     :
 done
 
 currenttime=$(date +%s)
-curl  -X POST -H "Content-type: application/json" -d "{ \"series\" : [{\"metric\":\"docker.pull.dockerhub\", \"points\":[[$currenttime, $count]], \"type\":\"count\", \"host\":\"test.example.com\", \"tags\":[\"environment:test\"]} ]  }" 'https://app.datadoghq.com/api/v1/series?api_key=45814898a6681cc7e7e10086d3cae350'
+curl  -X POST -H "Content-type: application/json" -d "{ \"series\" : [{\"metric\":\"docker.pull.dockerhub\", \"points\":[[$currenttime, $count]], \"type\":\"count\", \"host\":\"test.example.com\", \"tags\":[\"environment:test\"]} ]  }" "httyps://app.datadoghq.com/api/v1/series?api_key=$2"
