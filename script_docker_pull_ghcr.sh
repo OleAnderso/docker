726c59ce855579370bd14c434f0828bb77749c22
OleAnderso <101440312+OleAnderso@users.noreply.github.com>
2022-03-12 23:00:51 +0100

    Update script_docker_pull_ghcr.sh

--- script_docker_pull_ghcr.sh
+++ script_docker_pull_ghcr.sh
@@ -4,12 +4,12 @@ end=$((SECONDS+180+random_number))
 echo "RunCount: $end"
 count=0
 while [ $SECONDS -lt $end ]; do
-    docker pull ghcr.io/michaelknightdriver/docker-nginx-brotli:latest
-    #docker image rm ghcr.io/michaelknightdriver/docker-nginx-brotli:latest
+    docker pull "ghcr.io/$1"
+    #docker image rm "ghcr.io/$1"
     ((count++))
     :
 done
 
 currenttime=$(date +%s)
-curl  -X POST -H "Content-type: application/json" -d "{ \"series\" : [{\"metric\":\"docker.pull.ghcr\", \"points\":[[$currenttime, $count]], \"type\":\"count\", \"host\":\"test.example.com\", \"tags\":[\"environment:test\"]} ]  }" 'https://app.datadoghq.com/api/v1/series?api_key=45814898a6681cc7e7e10086d3cae350'
+curl  -X POST -H "Content-type: application/json" -d "{ \"series\" : [{\"metric\":\"docker.pull.ghcr\", \"points\":[[$currenttime, $count]], \"type\":\"count\", \"host\":\"test.example.com\", \"tags\":[\"environment:test\"]} ]  }" "https://app.datadoghq.com/api/v1/series?api_key=$2"
 
